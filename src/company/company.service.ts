4306382e6236d05fd5d1407c55f2a0d39ef99000
Enrique <enriquemolina61@hotmail.com>
2024-03-18 20:21:46 -0300

    create company

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
-import { Company } from './entities/company.entity';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { CreateCompanyDto } from "./dto/create-company.dto";
+import { UpdateCompanyDto } from "./dto/update-company.dto";
+import { Company } from "./entities/company.entity";
+import { PrismaService } from "src/prisma/prisma.service";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class CompanyService {
-  create(createCompanyDto: CreateCompanyDto) {
-    throw new Error('Method not implemented.');
-  }
   constructor(private readonly prisma: PrismaService) {}
-  // create(createCompanyDto: CreateCompanyDto):Promise<Company> {
-  //   const data:Company = {...createCompanyDto, userId};
 
-  //   return this.prisma.company.create({data})
-  // }
+  create(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    const { ownerId, ...dataCompany } = createCompanyDto;
+    const data: Prisma.CompanyCreateInput = {
+      ...dataCompany,
+      owner: { connect: { id: ownerId } },
+    };
+
+    return this.prisma.company.create({ data });
+  }
 
   findAll() {
     return this.prisma.company.findMany();
@@ -25,7 +28,10 @@ export class CompanyService {
   }
 
   update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+    return `This action updates a #${id} company with ${JSON.stringify(
+      updateCompanyDto,
+    )}
+      `;
   }
 
   remove(id: number) {
--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,29 +1,36 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
 
 export class CreateCompanyDto {
-
   @IsNotEmpty()
   @IsString()
   @ApiProperty({
-    description: 'Nome da empresa',
-    example: 'Empresa XPTO',
+    description: "Nome da empresa",
+    example: "Empresa XPTO",
   })
   name: string;
 
   @IsNotEmpty()
   @IsString()
   @ApiProperty({
-    description: 'CNPJ da empresa',
-    example: '12345678901234',
+    description: "CNPJ da empresa",
+    example: "12345678901234",
   })
   cnpj: string;
 
   @IsNotEmpty()
   @IsString()
   @ApiProperty({
-    description: 'Endereço da empresa',
-    example: 'Rua XPTO, 123',
+    description: "Endereço da empresa",
+    example: "Rua XPTO, 123",
   })
   site: string;
+
+  @IsNotEmpty()
+  @IsUUID("all")
+  @ApiProperty({
+    description: "ID do usuário responsável pela empresa",
+    example: `123e4567-e89b-12d3-a456-426614174000`,
+  })
+  ownerId: string;
 }
