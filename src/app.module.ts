327b29d64864c66490df8ae735352b70efa7f348
Enrique <enriquemolina61@hotmail.com>
2024-03-19 21:49:41 -0300

    resource-plant

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CompanyModule } from './company/company.module';
+import { PlantModule } from './plant/plant.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, CompanyModule],
+  imports: [UserModule, PrismaModule, CompanyModule, PlantModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
