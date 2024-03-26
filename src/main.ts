e0b4a8b69bb978a20aa70bbb719618527fa06423
Enrique <enriquemolina61@hotmail.com>
2024-03-26 20:25:30 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Swagger
   const config = new DocumentBuilder()
-  .setTitle('Sweg')
-  .setDescription('Aplicação para gestão das usinas de energia elétrica.')
-  .setVersion('1.0.0')
-  .addTag('Status')
-  .addTag('Users')
-  .addTag('Company')
-  .addTag('Plant')
-  .build();
+    .setTitle("Sweg")
+    .setDescription("Aplicação para gestão das usinas de energia elétrica.")
+    .setVersion("1.0.0")
+    .addTag("Status")
+    .addTag("Users")
+    .addTag("Company")
+    .addTag("Plant")
+    .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  const port = process.env.PORT || 3333;
+  SwaggerModule.setup("api", app, document);
 
-  await app.listen(3333);
+  await app.listen(port, "3333");
 }
 bootstrap();
